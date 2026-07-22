a9a73797c4b90aff4b2098c1a72cecd5e710a8a6
salarcode <salar2k@gmail.com>
2026-07-22 16:48:33 +1000

    benchmark simplifications

---
+++ Salar.BinaryBuffers.Benchmarks/SimpleBenchmarks.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Salar.BinaryBuffers.Benchmarks;
+
+[DisassemblyDiagnoser(maxDepth: 3, exportCombinedDisassemblyReport: true)]
+public class BinaryBufferWriter_WriteInt32
+{
+	private readonly byte[] _buffer = new byte[sizeof(int)];
+	private readonly BinaryBufferWriter _writer;
+
+	public BinaryBufferWriter_WriteInt32()
+	{
+		_writer = new BinaryBufferWriter(_buffer);
+	}
+
+	[Benchmark]
+	public void WriteInt32()
+	{
+		_writer.Position = 0;
+		_writer.Write(0x12345678);
+	}
+}
